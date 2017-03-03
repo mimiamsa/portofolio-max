196d1b0ad3e0a0629a0745d4efeaa638822ae1f4
mimi <mi.amsalem@gmail.com>
2017-03-03 12:05:43 +0100

    pb nav regler

--- js/modules/gui_scroller.js
+++ js/modules/gui_scroller.js
@@ -3,21 +3,26 @@ define(function () {
     "use strict";
 
     function scrollAction() {
-        var navToFix, navMobile, navPosToFix, navPosMobile;
+        var navToFix, navMobile, navPosToFix, navPosMobile, contentImg;
         navToFix = byId('nav-fix');
         navMobile = byId('nav-mobile');
+        contentImg = select('.images-content');
         // log(navToFix);
         // log(navMobile);
+        log(contentImg);
         navPosToFix = navToFix.getBoundingClientRect().top;
         navPosMobile = navMobile.getBoundingClientRect().top;
         // log('——position de la nav fix: ' + navPosToFix + " ——position de la nav mobile: " + navPosMobile);
         if (navPosToFix <= 0) {
             navToFix.style.position = 'fixed';
             navToFix.style.top = "0"; //add class avec top 0
+            contentImg.classList.add('to-relative');
+
         }
         if (navPosMobile > 0) {
             if (navToFix.style.position = 'fixed') {
                 navToFix.style.position = "relative";
+                contentImg.classList.remove('to-relative');
             }
         }
     }
